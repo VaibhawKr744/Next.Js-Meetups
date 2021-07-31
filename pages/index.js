c7be310a5674ec6063ad07f545307a6f8837cbcc
VaibhawKr744 <honeykr744@gmail.com>
2021-07-31 18:04:53 +0530

    added meetup forms

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+ import MeetupList from '../components/meetups/MeetupList';
+
+ const DUMMY_MEETUPS = [
+     {
+         id:'m1',
+         title: 'A First Meetup',
+         image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+         address:' some add 1',
+         description:'This is a first meetup'
+     },
+     {
+        id:'m2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/d/da/Frauenkirche_Munich_-_View_from_Peterskirche_Tower2.jpg',
+        address:' some add 2',
+        description:'This is a second meetup'
+    }
+ ]
+ function HomePage() {
+     return <MeetupList meetups={DUMMY_MEETUPS} />
+ }
+
+ export default HomePage;
